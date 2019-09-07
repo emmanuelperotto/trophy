78a1484a4d5cb038dc80956634652930bcb5a727
Emmanuel Perotto <emmanuelperotto@gmail.com>
2019-09-07 20:34:58 -0300

    add more validations

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 class Reward < ApplicationRecord
+  # Files
+  has_one_attached :image
+
   # Associations
   has_many :reward_users, dependent: :destroy
   has_many :users, through: :reward_users
 
+  # Enums
+  enum type: %i[deaths collected_coins
+                killed_turtles killed_bowsers]
+
   # Validations
   validates :type, presence: true
   validates :required_value, presence: true,
-                             numericality: { greater_than_or_equal_to: 0 }
+                             numericality: { greater_than_or_equal_to: 0 },
+                             uniqueness: { scope: :type }
 end
--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -12,5 +12,6 @@ RSpec.describe Reward, type: :model do
     it { is_expected.to validate_presence_of(:type) }
     it { is_expected.to validate_presence_of(:required_value) }
     it { is_expected.to validate_numericality_of(:required_value).is_greater_than_or_equal_to(0) }
+    it { is_expected.to validate_uniqueness_of(:required_value).scoped_to(:type).ignoring_case_sensitivity }
   end
 end
