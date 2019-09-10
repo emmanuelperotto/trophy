bc5862ab3d25202ab8718a36a65bc8424ec2a272
Emmanuel Perotto <emmanuelperotto@gmail.com>
2019-09-09 23:48:47 -0300

    change rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -25,8 +25,10 @@ RailsAdmin.config do |config|
   # config.show_gravatar = true
 
   config.actions do
-    dashboard                     # mandatory
-    index                         # mandatory
+    dashboard do
+      except ['KilledMonster', 'KilledMonsters']
+    end
+    index
     new
     export
     bulk_delete
@@ -38,5 +40,83 @@ RailsAdmin.config do |config|
     ## With an audit adapter, you can add:
     # history_index
     # history_show
+  end
+
+  config.excluded_models = ['ActiveStorage::Attachment', 'ActiveStorage::Blob']
+
+  config.model 'Monster' do
+    list do
+      exclude_fields :killed_monsters, :users
+    end
+
+    show do
+      exclude_fields :users, :killed_monsters
+    end
+
+    edit do
+      exclude_fields :users, :killed_monsters
+    end
+
+    create do
+      exclude_fields :users, :killed_monsters
+    end
+  end
+
+  config.model 'Reward' do
+    config.label_methods << :description
+
+    list do
+      exclude_fields :reward_users
+    end
+
+    show do
+      exclude_fields :reward_users
+    end
+
+    edit do
+      exclude_fields :image, :reward_users, :users
+    end
+
+    create do
+      exclude_fields :image, :reward_users, :users
+    end
+  end
+
+  config.model 'User' do
+    config.label_methods << :email
+
+    list do
+      field :id
+      field :email
+      field :total_coins
+      field :deaths_count
+      field :killed_bowsers_count
+      field :killed_turtles_count
+      field :killed_monsters_count
+      field :created_at
+      field :updated_at
+    end
+
+    show do
+      field :email
+      field :total_coins
+      field :deaths_count
+      field :killed_bowsers_count
+      field :killed_turtles_count
+      field :killed_monsters_count
+      field :rewards
+    end
+
+    edit do
+      field :email
+      field :password
+      field :password_confirmation
+    end
+
+    create do
+      field :email
+      field :password
+      field :password_confirmation
+    end
   end
 end
